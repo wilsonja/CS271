5cc7bcc1a012a31e4eced6ff75695c74d6a1745e
Jacob Wilson <wilsonja117@gmail.com>
2016-08-22 13:36:58 -0700

    Update Prog01.asm

--- Project-1/Prog01.asm
+++ Project-1/Prog01.asm
@@ -14,9 +14,9 @@ INCLUDE Irvine32.inc
 .data
 
 myName		BYTE	"by Jacob Wilson", 0
-intro		  BYTE	9,"Elementary Arithmetic", 9, 9, 0
+intro		BYTE	9,"Elementary Arithmetic", 9, 9, 0
 prompt_1	BYTE	"Enter two numbers and I'll show you the sum, difference,", 13,10
-			    BYTE	"product, quotient, and remainder.", 0
+		BYTE	"product, quotient, and remainder.", 0
 ec2Prompt	BYTE	"**EC: Program verfies that second number is less than first.",0
 firstNum	BYTE	"First number: ", 0
 secondNum	BYTE	"Second number: ", 0
@@ -32,8 +32,8 @@ areEqual	BYTE	"The two numbers are equal!", 0
 goodbye		BYTE	"Hope you had fun! Goodbye!", 0
 enter_1		DWORD	?
 enter_2		DWORD	?
-sum			  DWORD	?
-diff		  DWORD	?
+sum		DWORD	?
+diff		DWORD	?
 product		DWORD	?
 quot	  	DWORD	?
 remain		DWORD	?
@@ -42,145 +42,145 @@ remain		DWORD	?
 main PROC
 
 ; introduction
-	mov	  	edx, OFFSET intro
+	mov	edx, OFFSET intro
 	call  	WriteString
-	mov		  edx, OFFSET	myName
-	call	  WriteString
+	mov	edx, OFFSET	myName
+	call	WriteString
 	call  	CrLf
 	call  	CrLf
-	mov		  edx, OFFSET ec2Prompt
-	call	  WriteString
-	call	  CrLf
-	call	  CrLf
+	mov	edx, OFFSET ec2Prompt
+	call	WriteString
+	call	CrLf
+	call	CrLf
 
 ; get the data
-	mov	  	edx, OFFSET prompt_1
+	mov	edx, OFFSET prompt_1
 	call  	WriteString
-	call	  CrLf
-	call	  CrLf
-	mov	  	edx, OFFSET firstNum
-	call	  WriteString
-	call	  ReadInt				            ; read in first number
-	mov	  	enter_1, eax		          ; store first number
-	mov	  	edx, OFFSET secondNum
-	call	  WriteString
-	call	  ReadInt				            ; read in second number
-	mov		  enter_2, eax		          ; store second number
-	call	  CrLf
+	call	CrLf
+	call	CrLf
+	mov	edx, OFFSET firstNum
+	call	WriteString
+	call	ReadInt				; read in first number
+	mov	enter_1, eax			; store first number
+	mov	edx, OFFSET secondNum
+	call	WriteString
+	call	ReadInt				; read in second number
+	mov	enter_2, eax			; store second number
+	call	CrLf
 
 ; calculate the required values
 	; addition
-	mov	  	eax, enter_1
-	add		  eax, enter_2
-	mov		  sum, eax
+	mov	eax, enter_1
+	ad	eax, enter_2
+	mov	sum, eax
 
 	; subtraction
-	mov		  eax, enter_1
-	sub	  	eax, enter_2
-	mov		  diff, eax
+	mov	eax, enter_1
+	sub	eax, enter_2
+	mov	diff, eax
 
 	; mutliplication
-	mov		  eax, enter_1
-	mov	  	ebx, enter_2
-	mul	  	ebx
-	mov		  product, eax
+	mov	eax, enter_1
+	mov	ebx, enter_2
+	mul	ebx
+	mov	product, eax
 
 	; division
-	mov	  	eax, enter_1
-	mov	  	ebx, enter_2
-	div	  	ebx
-	mov		  quot, eax
-	mov		  remain, edx
+	mov	eax, enter_1
+	mov	ebx, enter_2
+	div	ebx
+	mov	quot, eax
+	mov	remain, edx
 
 ; display the results
 	; display addition
-	mov	  	eax, enter_1
-	call	  WriteDec
-	mov		  edx, OFFSET plusSign
+	mov	eax, enter_1
+	call	WriteDec
+	mov	edx, OFFSET plusSign
 	call  	WriteString
-	mov	  	eax, enter_2
+	mov	eax, enter_2
 	call  	WriteDec
-	mov		  edx, OFFSET eqSign
+	mov	edx, OFFSET eqSign
 	call  	WriteString
-	mov	  	eax, sum
-	call	  WriteDec
-	call	  CrLf
+	mov	eax, sum
+	call	WriteDec
+	call	CrLf
 
 	; display subtraction
-	mov		  eax, enter_1
-	call	  WriteDec
-	mov		  edx, OFFSET minusSign
-	call	  WriteString
-	mov		  eax, enter_2
+	mov	eax, enter_1
+	call	WriteDec
+	mov	edx, OFFSET minusSign
+	call	WriteString
+	mov	eax, enter_2
 	call  	WriteDec
-	mov		  edx, OFFSET eqSign
+	mov	edx, OFFSET eqSign
 	call  	WriteString
-	mov		  eax, diff
-	call	  WriteInt
-	call	  CrLf
+	mov	eax, diff
+	call	WriteInt
+	call	CrLf
 
 	; display multiplication
-	mov		  eax, enter_1
+	mov	eax, enter_1
 	call  	WriteDec
-	mov	  	edx, OFFSET multSign
+	mov	edx, OFFSET multSign
 	call  	WriteString
-	mov	  	eax, enter_2
+	mov	eax, enter_2
 	call  	WriteDec
-	mov		  edx, OFFSET eqSign
+	mov	edx, OFFSET eqSign
 	call  	WriteString
-	mov		  eax, product
+	mov	eax, product
 	call  	WriteDec
-	call	  CrLf
+	call	CrLf
 
 	; display division with remainder
-	mov		  eax, enter_1
+	mov	eax, enter_1
 	call  	WriteDec
-	mov		  edx, OFFSET divSign
-	call	  WriteString
-	mov	  	eax, enter_2
+	mov	edx, OFFSET divSign
+	call	WriteString
+	mov	eax, enter_2
 	call  	WriteDec
-	mov		  edx, OFFSET eqSign
-	call	  WriteString
-	mov	  	eax, quot				          ; for the quotient
+	mov	edx, OFFSET eqSign
+	call	WriteString
+	mov	eax, quot			; for the quotient
 	call  	WriteDec
-	mov		  edx, OFFSET remSign
-	call	  WriteString
-	mov	  	eax, remain			        	; for the remainder
+	mov	edx, OFFSET remSign
+	call	WriteString
+	mov	eax, remain			; for the remainder
 	call  	WriteDec
 	call  	CrLf
-	call	  CrLf
+	call	CrLf
 
 ; determine if second number is greater of less
-	mov		  eax, enter_1
-	cmp	  	eax, enter_2
-	jg		  L1
-	jl		  L2
-	je		  L3
+	mov	eax, enter_1
+	cmp	eax, enter_2
+	jg	L1
+	jl	L2
+	je	L3
 L1:
 	; if second number is greater
-	mov		  edx, OFFSET isGreater
-	call	  WriteString
-	call	  CrLf
-	jmp		  Bottom
+	mov	edx, OFFSET isGreater
+	call	WriteString
+	call	CrLf
+	jmp	Bottom
 L2:
 	; if second number is less than
-	mov		  edx, OFFSET isLess
+	mov	edx, OFFSET isLess
 	call  	WriteString
-	call	  CrLf
-	jmp		  Bottom
+	call	CrLf
+	jmp	Bottom
 L3:
 	; if both numbers are equal
-	mov	  	edx, OFFSET areEqual
-	call	  WriteString
-	call	  CrLf
+	mov	edx, OFFSET areEqual
+	call	WriteString
+	call	CrLf
 
 ; say goodbye
 Bottom:
 	call  	CrLf
-	mov	  	edx, OFFSET goodbye
+	mov	edx, OFFSET goodbye
 	call  	WriteString
-	call	  CrLf
-	call	  CrLf
+	call	CrLf
+	call	CrLf
 
 	exit	; exit to operating system
 main ENDP
