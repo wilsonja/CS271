4a8c05eb51ff6200de741a431ce83d18ee74166e
Jacob Wilson <wilsonja117@gmail.com>
2016-08-22 13:47:20 -0700

    Update Prog04.asm

--- Project-4/Prog04.asm
+++ Project-4/Prog04.asm
@@ -14,22 +14,22 @@ LOWERLIMIT = 1
 UPPERLIMIT = 400
 
 .data
-progTitle		BYTE	"Composite Numbers            Programmed by Jacob Wilson", 0
+progTitle	BYTE	"Composite Numbers            Programmed by Jacob Wilson", 0
 showInstruct	BYTE	"Enter the number of composite numbers you would like to see.",13, 10
-				BYTE	"I can display up to 400 composites.", 0
-askNums			BYTE	"Enter the number of composites to display [1 ... 400]: ", 0
+		BYTE	"I can display up to 400 composites.", 0
+askNums		BYTE	"Enter the number of composites to display [1 ... 400]: ", 0
 showOutRange	BYTE	"Out of range. Try again.", 0
-sayGoodBye		BYTE	"Results certified by Jacob W.   Goodbye!", 0
-ecPrompt		BYTE	"**EC: Align the output columns.", 0
-spaces3			BYTE	32, 32, 32, 0
-spaces4			BYTE	32, 32, 32, 32, 0
-spaces5			BYTE	32, 32, 32, 32, 32, 0
+sayGoodBye	BYTE	"Results certified by Jacob W.   Goodbye!", 0
+ecPrompt	BYTE	"**EC: Align the output columns.", 0
+spaces3		BYTE	32, 32, 32, 0
+spaces4		BYTE	32, 32, 32, 32, 0
+spaces5		BYTE	32, 32, 32, 32, 32, 0
 
-numComps		DWORD	?								; number of composites requested by user
-currentComp		DWORD	?								; current number being tested
-lineCount		DWORD	10								; decrements, line break needed when equal 0
-inRange			DWORD	0								; acts as a bool variable, 1 when true
-compFound		DWORD	0								; acts as a bool variable, 1 when true
+numComps	DWORD	?					; number of composites requested by user
+currentComp	DWORD	?					; current number being tested
+lineCount	DWORD	10					; decrements, line break needed when equal 0
+inRange		DWORD	0					; acts as a bool variable, 1 when true
+compFound	DWORD	0					; acts as a bool variable, 1 when true
 
 .code
 main PROC
@@ -52,16 +52,16 @@ main ENDP
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 introduction PROC
 	; display program title information
-	mov		edx, OFFSET progTitle
+	mov	edx, OFFSET progTitle
 	call	WriteString
 	call	Crlf
-	mov		edx, OFFSET ecPrompt
+	mov	edx, OFFSET ecPrompt
 	call	WriteString
 	call	Crlf
 	call	Crlf
 
 	; display user instructions
-	mov		edx, OFFSET showInstruct
+	mov	edx, OFFSET showInstruct
 	call	WriteString
 	call	Crlf
 	call	Crlf
@@ -81,14 +81,14 @@ introduction ENDP
 getUserData PROC
 getNums:
 	; prompt the user to enter a number
-	mov		edx, OFFSET askNums
+	mov	edx, OFFSET askNums
 	call	WriteString
 	call	ReadInt
-	mov		numComps, eax								; store the user inputted number
-	call	validate									; validate the number
-	mov		eax, 0
-	cmp		eax, inRange								; inRange = 1 (true) when valid
-	je		getNums										; request a new number if invalid
+	mov	numComps, eax					; store the user inputted number
+	call	validate					; validate the number
+	mov	eax, 0
+	cmp	eax, inRange					; inRange = 1 (true) when valid
+	je	getNums						; request a new number if invalid
 	ret
 getUserData ENDP
 
@@ -104,22 +104,22 @@ getUserData ENDP
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 validate PROC
 	; check the number against the lower limit
-	mov		eax, LOWERLIMIT
-	cmp		numComps, eax
-	jl		OutRange
+	mov	eax, LOWERLIMIT
+	cmp	numComps, eax
+	jl	OutRange
 
 	; check the number against the upper limit
-	mov		eax, UPPERLIMIT
-	cmp		numComps, eax
-	jg		OutRange
+	mov	eax, UPPERLIMIT
+	cmp	numComps, eax
+	jg	OutRange
 
 	; when number is in range
-	mov		inRange, 1									; inRange = 1 (true) when valid
-	jmp		ValDone
+	mov	inRange, 1					; inRange = 1 (true) when valid
+	jmp	ValDone
 
 OutRange:
 	; display message if the number is out of range
-	mov		edx, OFFSET showOutRange
+	mov	edx, OFFSET showOutRange
 	call	WriteString
 	call	Crlf
 
@@ -143,53 +143,46 @@ validate ENDP
 showComposites PROC
 	; initialize values for isComposite call
 	call	Crlf
-	mov		eax, 4										; set to lowest composite number
-	mov		currentComp, eax							; begin with lowest possible composite
-	mov		ecx, numComps								; outer loop set to user requested numbers
+	mov	eax, 4						; set to lowest composite number
+	mov	currentComp, eax				; begin with lowest possible composite
+	mov	ecx, numComps					; outer loop set to user requested numbers
 
 CompDisplay:
 	; reset eax and ebx to prepare for isComposite call
-	mov		eax, currentComp	
-	mov		ebx, 2										; lowest divisor for composite numbers
+	mov	eax, currentComp	
+	mov	ebx, 2						; lowest divisor for composite numbers
 	call	isComposite
-	cmp		compFound, 0								; compFound = 1 (true) when composite number found
-	je		CompNotFound								; restart loop with new value if not composite
+	cmp	compFound, 0					; compFound = 1 (true) when composite number found
+	je	CompNotFound					; restart loop with new value if not composite
 
 	; display value after finding a composite number
-	mov		eax, currentComp
+	mov	eax, currentComp
 	call	WriteDec
-	inc		currentComp
+	inc	currentComp
 
 	; check to see if a line break is necessary
-	dec		lineCount
-	mov		eax, 0
-	cmp		lineCount, eax
-	je		AddLine
-	;jne		AddSpaces
-	call	addSpaces									; additional procedure to align output columns
-	jmp		ShowDone
+	dec	lineCount
+	mov	eax, 0
+	cmp	lineCount, eax
+	je	AddLine
+	call	addSpaces					; additional procedure to align output columns
+	jmp	ShowDone
 
 AddLine:
 	; add line, reset lineCount to 10
 	call	Crlf
-	mov		lineCount, 10
-	jmp		ShowDone
-
-;AddSpaces:
-	; if new line not necessary, add spaces
-	;mov		edx, OFFSET spaces3
-	;call	WriteString
-	;jmp		ShowDone
+	mov	lineCount, 10
+	jmp	ShowDone
 
 CompNotFound:
 	; check additional numbers as composites
-	inc		currentComp									; move on to next number for testing
-	jmp		CompDisplay									; loop count not incremented if composite not found
+	inc		currentComp				; move on to next number for testing
+	jmp		CompDisplay				; loop count not incremented if composite not found
 
 ShowDone:
 	; reset compFound to false and resume loop
-	mov		compFound, 0
-	loop	CompDisplay									; loop count only increments when composite is found
+	mov	compFound, 0
+	loop	CompDisplay					; loop count only increments when composite is found
 showComposites ENDP
 
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
@@ -208,24 +201,24 @@ showComposites ENDP
 isComposite PROC
 FindComp:
 	; compare the current number with divisor, if equal move on to next number
-	cmp		ebx, eax
-	je		CompDone
+	cmp	ebx, eax
+	je	CompDone
 
 	; check divisibility, if remainder = 0, the number is composite
 	cdq
-	div		ebx
-	cmp		edx, 0
-	je		IsComp										; composite found
+	div	ebx
+	cmp	edx, 0
+	je	IsComp						; composite found
 
 CheckComp:
 	; check current number against additional divisors
-	mov		eax, currentComp
-	inc		ebx
-	jmp		FindComp
+	mov	eax, currentComp
+	inc	ebx
+	jmp	FindComp
 
 IsComp:
 	; once a composite number has been found
-	mov		compFound, 1
+	mov	compFound, 1
 
 CompDone:
 	ret
@@ -242,17 +235,17 @@ isComposite ENDP
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 farewell PROC
 	; skip additional line break if number of composites is factor of 10
-	mov		eax, numComps
-	mov		ebx, 10
+	mov	eax, numComps
+	mov	ebx, 10
 	cdq
-	div		ebx
-	cmp		edx, 0
-	je		SkipLine
+	div	ebx
+	cmp	edx, 0
+	je	SkipLine
 	call	Crlf
 
 SkipLine:
 	call	Crlf
-	mov		edx, OFFSET sayGoodBye
+	mov	edx, OFFSET sayGoodBye
 	call	WriteString
 	call	Crlf
 	ret
@@ -269,20 +262,20 @@ farewell ENDP
 ; Registers changed: eax, edx
 ; ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 addSpaces PROC
-	mov		eax, currentComp
-	cmp		eax, 10
-	jle		Space5
-	cmp		eax, 100
-	jle		Space4
-	mov		edx, OFFSET spaces3
+	mov	eax, currentComp
+	cmp	eax, 10
+	jle	Space5
+	cmp	eax, 100
+	jle	Space4
+	mov	edx, OFFSET spaces3
 	call	WriteString
 	ret
 Space5:
-	mov		edx, OFFSET spaces5
+	mov	edx, OFFSET spaces5
 	call	WriteString
 	ret
 Space4:
-	mov		edx, OFFSET spaces4
+	mov	edx, OFFSET spaces4
 	call	WriteString
 	ret
 addSpaces ENDP
